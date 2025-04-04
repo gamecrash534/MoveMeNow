40ab136d4f0402a4c6ff7459b3b99115fbe9b86c
game.crash <73080127+gamecrash534@users.noreply.github.com>
2025-04-04 18:58:22 +0200

    Implement tab completion for ReloadCommand

--- src/main/java/net/craftminecraft/bungee/movemenow/ReloadCommand.java
+++ src/main/java/net/craftminecraft/bungee/movemenow/ReloadCommand.java
@@ -1,11 +1,12 @@
 package net.craftminecraft.bungee.movemenow;
 
-import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Command;
+import net.md_5.bungee.api.plugin.TabExecutor;
+import java.util.ArrayList;
 
-public class ReloadCommand extends Command {
+public class ReloadCommand extends Command implements TabExecutor {
     MoveMeNow plugin;
 
     public ReloadCommand(MoveMeNow plugin) {
@@ -22,5 +23,13 @@ public class ReloadCommand extends Command {
             case "reload":
                 plugin.loadConfig();
         }
+    }
+
+    @Override
+    public Iterable<String> onTabComplete(CommandSender sender, String[] args)
+    {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("reload");
+        return list;
     }
 }
